5ff049e7b6cd5234563c1ad7a58125fe79d8624c
Ruan-Miguel <miguelcoelho148@gmail.com>
2020-12-10 18:05:21 -0300

    aula 4

--- App.tsx
+++ App.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import { AppLoading } from "expo";
+import { useFonts } from "expo-font";
+import { Nunito_600SemiBold, Nunito_700Bold, Nunito_800ExtraBold } from "@expo-google-fonts/nunito";
 
 import Routes from "./src/routes";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Nunito_600SemiBold,
+    Nunito_700Bold,
+    Nunito_800ExtraBold
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading/>
+  }
+
   return (
     <Routes />
   );
--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,8 +1,42 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet, Dimensions } from 'react-native';
+import MapView, { PROVIDER_GOOGLE, Marker } from "react-native-maps";
+
+import mapMarker from "../images/map-marker.png";
 
 const Orphanagesmap = () => {
-  return <View />;
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.chart}
+        provider={PROVIDER_GOOGLE}
+        initialRegion={{
+          latitude: -6.5205485,
+          longitude: -38.4155765,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008,
+        }}
+      >
+        <Marker
+          icon={mapMarker}
+          coordinate={{
+            latitude: -6.5205485,
+            longitude: -38.4155765,
+          }}
+        ></Marker>
+      </MapView>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  chart: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+})
 
 export default Orphanagesmap;
